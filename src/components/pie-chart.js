93a88bc3e90c3936bb6c04f22f6179be88f33f37
zhangjing <zhangjing@ilinkin.com.cn>
2018-07-26 20:48:07 +0800

    update

---
+++ src/components/pie-chart.js
@@ -0,0 +1,15 @@
+import { Pie } from 'vue-chartjs'
+
+export default {
+  extends: Pie,
+  props: ['data'],
+  mounted() {
+    this.renderChart(this.data, {
+      responsive: true,
+      maintainAspectRatio: false,
+      legend: {
+        position: 'left'
+      }
+    })
+  }
+}
